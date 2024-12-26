c17b8ccd0ac61aa5336558b5b6f7d24b546a832e
marine-ecologist <george.roff@csiro.au>
2024-12-27 06:34:33 +1000

    Increment version number to 0.0.0.9009

--- R/download_gbr_spatial.R
+++ R/download_gbr_spatial.R
@@ -11,6 +11,7 @@
 #'
 #'
 #' @param crs change the CRS if needed (EPSG:4283 as default)
+#' @param reefs Combine reefs to single LABEL_ID? See vignette
 #' @returns Simple feature collection with 9612 features and 35 fields
 #' @examples
 #' \dontrun{
@@ -20,7 +21,7 @@
 
 
 
-download_gbr_spatial <- function(crs = "EPSG:4283") {
+download_gbr_spatial <- function(crs = "EPSG:4283", combine) {
   # URL for the spatial data
   url <- "https://nextcloud.eatlas.org.au/s/xQ8neGxxCbgWGSd/download/TS_AIMS_NESP_Torres_Strait_Features_V1b_with_GBR_Features.zip"
 
@@ -56,5 +57,36 @@ download_gbr_spatial <- function(crs = "EPSG:4283") {
   gbr_shape <- sf::st_read(shapefile_path, quiet = TRUE) |>
     sf::st_transform(crs)
 
+  if (reefs=="combined"){
+
+    gbr_files <- gbr_shape |> st_transform(4283)
+    gbr_files$LABEL_ID_FULL <- gbr_files$LABEL_ID
+    gbr_files$LABEL_ID <- stringr::str_extract(gbr_files$LABEL_ID_FULL, "^\\d{2}-\\d{3}")
+
+    label_ids <- gbr_files |>
+      filter(FEAT_NAME %in% c("Reef", "Terestrial Reef")) |>
+      as.data.frame() |>
+      select(LABEL_ID, GBR_NAME) |>
+      group_by(LABEL_ID) |>
+      filter(row_number() == 1)
+    #filter(GBR_NAME==first(GBR_NAME))
+
+
+    gbr_shape <- gbr_files |>
+      filter(FEAT_NAME %in% c("Reef", "Terestrial Reef")) |>
+      st_make_valid() |>
+      group_by(LABEL_ID) |>
+      summarize(geometry = st_union(geometry)) |>
+      ungroup() |>
+      left_join(label_ids, by="LABEL_ID") %>%
+      mutate(area = st_area(.))
+
+    return(gbr_shape)
+
+  } else {
+
   return(gbr_shape)
+
+  }
+
 }
